2d8d9236d1c8cfcb68bd8218d9ff62b97426e773
Ahmed Gamal <victorylover987@gmail.com>
2024-03-17 22:48:15 -0400

    modified deployment.yaml file to accomodate for v9

--- application/start.sh
+++ application/start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# Dynamically construct the DATABASE_URL from individual components
+export DATABASE_URL="postgresql://${db_username}:${db_password}@${POSTGRES_HOST}:5432/${db_name}"
+
+# Debug: Echo the DATABASE_URL to verify it's correctly constructed
+echo "Constructed DATABASE_URL: $DATABASE_URL"
+
 # Now run the original CMD
 exec "$@"
