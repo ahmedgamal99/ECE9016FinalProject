c9b0d547230155328459678efe0896d90029c1d8
Ahmed Gamal <victorylover987@gmail.com>
2024-03-17 21:53:15 -0400

    modified development.yaml file to consider v4 and new database url and the sh file

--- application/start.sh
+++ application/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Dynamically construct the DATABASE_URL from individual components
-export DATABASE_URL=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres-service:5432/$POSTGRES_DB
+export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}"
+
+# Debug: Echo the DATABASE_URL to verify it's correctly constructed
+echo "Constructed DATABASE_URL: $DATABASE_URL"
 
 # Now run the original CMD
 exec "$@"
