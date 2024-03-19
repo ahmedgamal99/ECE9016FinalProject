6650119adead120ee81c270a4a1bd5c8cb78ee05
Ahmed Gamal <victorylover987@gmail.com>
2024-03-18 23:09:03 -0400

    prepped for version 13

--- application/start.sh
+++ application/start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-
 # Dynamically construct the DATABASE_URL from individual components
-export DATABASE_URL="postgresql://postgres:X9z@4bV!eU2wM8yP@postgres-service:5432/postgres-db"
+export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}"
 
-# Debug: Echo the DATABASE_URL to verify it's correctly constructed
+# Debugging: Print the constructed DATABASE_URL
 echo "Constructed DATABASE_URL: $DATABASE_URL"
 
-# Now run the original CMD
-exec "$@"
+# Start the Node application
+npm run start
