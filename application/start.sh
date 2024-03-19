4dd61f3b19efd18cee96c31f3b3fa391eca2f4aa
Ahmed Gamal <victorylover987@gmail.com>
2024-03-18 23:24:31 -0400

    prepped for version 14

--- application/start.sh
+++ application/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Dynamically construct the DATABASE_URL from individual components
-export DATABASE_URL="postgresql://${db_username }:${db_password}@${POSTGRES_HOST}:5432/${db_name}"
+export DATABASE_URL="postgresql://${db_username}:${db_password}@${POSTGRES_HOST}:5432/${db_name}"
 
 # Debugging: Print the constructed DATABASE_URL
 echo "Constructed DATABASE_URL: $DATABASE_URL"
